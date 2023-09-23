c6f3afd1e4b7022334fc585912f727eee3152250
AMEDOMEY Roméo David <davidromeoamedomey@yahoo.com>
2023-09-23 06:19:10 +0200

    feat(bff): creation of the project

---
+++ bff-backend/src/main/java/fr/unice/politech/borneappetit/BorneappetitApplication.java
@@ -0,0 +1,11 @@
+package fr.unice.politech.borneappetit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BorneappetitApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(BorneappetitApplication.class, args);
+	}
+}

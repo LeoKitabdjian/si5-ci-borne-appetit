af58cbbadb4341036202b4893e686c13e84591b9
Thomas <thomas.farineau@icloud.com>
2023-09-28 11:36:36 +0200

    Revert "Merge remote-tracking branch 'origin/front-api' into main"
    This reverts commit 76bae51d11e4c4eda70cc0039858bfd396bffeb0.

--- front/src/app/App.tsx
+++ front/src/app/App.tsx
@@ -3,7 +3,6 @@ import styles from "./App.module.sass";
 import {BrowserRouter, Navigate, Route, Routes} from "react-router-dom";
 import Standby from "../pages/Standby/Standby";
 import Ordering from "../pages/Ordering/Ordering";
-import TableNumber from "../pages/TableNumber/TableNumber";
 
 const App = () => {
     return (<div className={styles.App}>
@@ -11,7 +10,6 @@ const App = () => {
             <Routes>
                 <Route path="/" element={<Standby/>}/>
                 <Route path="/ordering" element={<Ordering/>}/>
-                <Route path="/tableNumber" element={<TableNumber/>}/>
                 <Route path='*' element={<Navigate to='/'/>}/>
             </Routes>
         </BrowserRouter>

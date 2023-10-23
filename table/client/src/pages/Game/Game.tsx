3ad51a7ce955d760ad356790fab50e214281f4d3
LeoKitabdjian <leo.kitabdjian06@gmail.com>
2023-10-23 12:16:39 +0200

    add game polling skeleton

--- table/client/src/pages/Game/Game.tsx
+++ table/client/src/pages/Game/Game.tsx
@@ -4,10 +4,16 @@ import {t} from "i18next";
 
 interface GameProps {}
 
-const Game: FC<GameProps> = () => (
-  <div className={styles.Game}>
-      <h2>{t('game.welcome')}</h2>
-  </div>
-);
+const POLL_INTERVAL = 5;
+
+const Game: FC<GameProps> = () => {
+    const poll = setInterval(() => {
+        console.log("polling");
+    }, POLL_INTERVAL * 1000);
+
+    return <div className={styles.Game}>
+        <h2>{t('game.welcome')}</h2>
+    </div>
+};
 
 export default Game;

1651b68a54e81de75f295e20d3c3db5494de3f22
AMEDOMEY Romeo David <davidromeoamedomey@yahoo.com>
2023-10-25 04:30:24 +0200

    feat: add client orders

--- bff-backend/src/main/java/fr/unice/politech/borneappetit/service/OrderService.java
+++ bff-backend/src/main/java/fr/unice/politech/borneappetit/service/OrderService.java
@@ -1,27 +1,29 @@
 package fr.unice.politech.borneappetit.service;
 
+import fr.unice.politech.borneappetit.dto.ClientOrderDto;
 import fr.unice.politech.borneappetit.dto.MenuDto;
 import fr.unice.politech.borneappetit.dto.OrderDto;
+import fr.unice.politech.borneappetit.model.ClientOrderEntity;
 import fr.unice.politech.borneappetit.model.Table;
 import fr.unice.politech.borneappetit.model.TableOrder;
 
+import fr.unice.politech.borneappetit.repository.ClientOrderRepository;
 import org.springframework.stereotype.Service;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @Service
 public class OrderService {
     private final TableService tableService;
     private final MenuService menuService;
-    private Map<String, OrderDto[]> orderlist;
 
-    public OrderService(TableService tableService, MenuService menuService) {
+    private final ClientOrderRepository clientOrderRepository;
+
+    public OrderService(TableService tableService, MenuService menuService, ClientOrderRepository clientOrderRepository) {
         this.tableService = tableService;
         this.menuService = menuService;
+        this.clientOrderRepository = clientOrderRepository;
     }
 
     public List create(OrderDto order) throws Exception {
@@ -53,64 +55,47 @@ public class OrderService {
         }
     }
 
-    public void storeOrder(OrderDto order, String tableId){
-        if (this.orderlist.containsKey(tableId)){
-            OrderDto[] orders = this.orderlist.get(tableId);
-            OrderDto[] newOrders = new OrderDto[orders.length + 1];
-            for (int i = 0; i < orders.length; i++){
-                newOrders[i] = orders[i];
-            }
-            newOrders[orders.length] = order;
-            this.orderlist.put(tableId, newOrders);
-        }
-        else{
-            orderlist.put(tableId, new OrderDto[]{order});
-        }
-    }
+    /**
+     * Create the client order or update it if there is one and not ordered
+     */
+    public ClientOrderEntity addOrUpdateClientOrder(ClientOrderDto orderDto) {
+        Optional<ClientOrderEntity> existingOrder = clientOrderRepository.findNotOrderedByTableIdAndClientId(orderDto.getTable(), orderDto.getClient());
 
-    public void storeOrder(OrderDto order, String tableId, int index){
-        if (this.orderlist.containsKey(tableId)){
-            OrderDto[] orders = this.orderlist.get(tableId);
-            orders[index] = order;
-            this.orderlist.put(tableId, orders);
-        }
-        else{
-            orderlist.put(tableId, new OrderDto[]{order});
-        }
-    }
-
-    private OrderDto mergeOrderDto(String tableId){
-        OrderDto[] orders = this.orderlist.get(tableId);
-        OrderDto order = new OrderDto();
-        for (int i = 0; i < orders.length; i++){
-            for (Map.Entry<String, Integer> entry : orders[i].items.entrySet()) {
-                String menuId = entry.getKey();
-                Integer howMany = entry.getValue();
-                if (order.items.containsKey(menuId)){
-                    order.items.put(menuId, order.items.get(menuId) + howMany);
-                }
-                else{
-                    order.items.put(menuId, howMany);
-                }
+        if (existingOrder.isPresent()) {
+            ClientOrderEntity orderToUpdate = existingOrder.get();
+            if (!orderToUpdate.isOrdered()) {
+                orderToUpdate.setItems(orderDto.getItems());
+                return clientOrderRepository.save(orderToUpdate);
             }
         }
-        return order;
-    
+
+        ClientOrderEntity newOrder = ClientOrderEntity.fromDto(orderDto);
+        return clientOrderRepository.save(newOrder);
     }
 
-    public List createTableOrder(String tableID){
-        OrderDto order = mergeOrderDto(tableID);
-        Table table = tableService.findByID(tableID);
-        TableOrder tableOrder = tableService.makeReservation(Math.toIntExact(table.getNumber()), Math.toIntExact(order.customer));
-        String tableOrderId = String.valueOf(tableOrder.getId());
-        Map<String, MenuDto> menusMap = Arrays.stream(this.menuService.getAll()).collect(Collectors.toMap(MenuDto::getId, menu -> menu));
-        for (Map.Entry<String, Integer> entry : order.items.entrySet()) {
-            String menuId = entry.getKey();
-            Integer howMany = entry.getValue();
-            MenuDto menu = menusMap.get(menuId);
-            tableService.addItemToTable(tableOrderId, menuId, menu.getShortName(), howMany);
+    public Object getOrderForTable(Long tableId) {
+        OrderDto orderDto = new OrderDto();
+
+        List<ClientOrderEntity> unordered = this.clientOrderRepository.findNotOrderedOrdersForTable(tableId);
+
+        Map response = new HashMap<>();
+
+        response.put("customers", unordered.stream()
+                .map(ClientOrderEntity::getClientId)
+                .distinct()
+                .count());
+        response.put("items", unordered);
+
+        return response;
+    }
+
+    public void markUnorderedOrdersAsOrdered(Long tableId) {
+        List<ClientOrderEntity> unorderedOrders = clientOrderRepository.findNotOrderedOrdersForTable(tableId);
+
+        for (ClientOrderEntity order : unorderedOrders) {
+            order.setOrdered(true);
         }
-        tableService.sendOrderToPreparation(tableOrderId);
-        return List.of(table.getNumber(), tableOrderId);
+
+        clientOrderRepository.saveAll(unorderedOrders);
     }
 }
